4ef1a3fe76e6689020534ebcf22a4df6818ad2e3
richliu(刘笑江) <richliu@tencent.com>
2018-02-02 11:28:08 +0800

    init

--- gen_audio_train.py
+++ gen_audio_train.py
@@ -33,22 +33,22 @@ def gen_train_audio(raw_dir, todo_dir):
                 # cut_audio(raw_dir / filename, todo_dir / 'none', '00:00:00', 5)
             print(raw_dir / filename)
 
-def gen_train_img(train_audio_dir, train_image_dir, bins):
+def gen_train_img(train_audio_dir, train_image_dir, window_ms, every_ms, bins, length=-1):
     for root, dirs, filenames in os.walk(train_audio_dir):
         if len(filenames) > 5:
             for filename in filenames:
                 if AUDIO_SUFFIX in filename:
                     image_path = (Path(root.replace(str(train_audio_dir), str(train_image_dir))) / filename).with_suffix(IMAGE_SUFFIX)
                     audio_path = Path(root) / filename
-                    if not image_path.exists():
-                        save_spectrogram_img(audio_path, image_path, bins)                
+                    save_spectrogram_img(audio_path, image_path, window_ms, every_ms, bins, length)
 
 if __name__ == '__main__':
-    raw_dir  = Path('./data/raw')
-    todo_dir = Path('./data/todo') # mp3 切片，人工分类 
-    train_audio_dir = Path('./data/train_audio')
-    train_image_dir = Path('./data/train_image')
+    raw_dir  = Path('data/raw')
+    todo_dir = Path('data/todo') # mp3 切片，人工分类 
+    train_audio_dir = Path('data/train_audio')
+    train_image_dir = Path('data/train_image')
 
     # gen_train_audio(raw_dir, todo_dir)
     # 手工分类
-    gen_train_img(train_audio_dir, train_image_dir, bins=64)
+    # gen_train_img(train_audio_dir, train_image_dir, window_ms=100, every_ms=50, bins=64, length=-1)
+    gen_train_img(train_audio_dir, train_image_dir, window_ms=25, every_ms=10, bins=64, length=101)
--- util_audio.py
+++ util_audio.py
@@ -9,12 +9,13 @@ import os
 from util import Util
 from PIL import Image
 import cv2
+from pathlib import Path
 
 def read_spectrogram_img(img_path):
     img = Image.open(img_path)
     return np.array(img)
 
-def save_spectrogram_img(audio_path, img_path, bins):
+def save_spectrogram_img(audio_path, img_path, window_ms, every_ms, bins=64, length=-1):
     print(f'save_spectrogram_img {audio_path} -> {img_path}')
     s = calc_spectrogram(audio_path, bins=bins)
     if s is None:
@@ -22,17 +23,28 @@ def save_spectrogram_img(audio_path, img_path, bins):
         return
     s_uint8 = (((s - s.min()) / (s.max() - s.min())) * 255.9).astype(np.uint8)
     Util.create_dir(os.path.dirname(img_path))
-    Image.fromarray(s_uint8).save(img_path)
+    if length == -1:
+        Image.fromarray(s_uint8).save(img_path)
+    else:
+        for i in range(int(s_uint8.shape[1] / length)):
+            p = i * length
+            q = (i + 1) * length
+            image_part = s_uint8[:, p:q]
+            if image_part.shape == (bins, length):
+                path = Path(img_path).with_suffix(f'.part{i}.png')
+                print(path)
+                Image.fromarray(image_part).save(path)
 
-def calc_spectrogram(audio_path, window_ms=100, every_ms=50, bins=64, plot=False, play=False):
+
+def calc_spectrogram(audio_path, window_ms=25, every_ms=10, bins=64, plot=False, play=False):
     try:
         y, sr = librosa.load(audio_path)
     except Exception as e:
         print(e)
         return None
     # applying xx ms window every yy ms
-    n_fft = int(window_ms / 1000 * sr)
-    hop_length = int(every_ms / 1000 * sr)
+    n_fft = int(window_ms * 1.0 / 1000 * sr)
+    hop_length = int(every_ms * 1.0 / 1000 * sr)
     s = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length)
     s = librosa.power_to_db(s, ref=np.max)
     s = cv2.resize(s, dsize=(s.shape[1], bins), interpolation=cv2.INTER_CUBIC)
@@ -64,13 +76,13 @@ def plot_spectrogram(audio_path, play=False):
         playsound.playsound(audio_path)
 
 if __name__ == '__main__':
-    pass
-    audio_path = 'data/train_audio/none/s0531rluyfo.sec105_5.mp3'
-    image_path = 'data/train_image/none/s0531rluyfo.sec105_5.png'
+
+    audio_path = 'data/train_audio/none/f0530m8fjco.p701.2.sec0_5.mp3'
+    image_path = 'data/train_image/none/f0530m8fjco.p701.2.sec0_5.png'
 
     # plot_spectrogram(audio_path, play=False)
-    calc_spectrogram(audio_path, plot=False, play=False)
+    # calc_spectrogram(audio_path, plot=False, play=False)
     # calc_spectrogram(audio_path, window_ms=25, every_ms=10, plot=True, play=True)
-    # save_spectrogram_img(audio_path, image_path)
+    save_spectrogram_img(audio_path, image_path, window_ms=25, every_ms=10, bins=64, length=101)
     # img = read_spectrogram_img(image_path)
     # print('image:', img.shape, img.dtype)
